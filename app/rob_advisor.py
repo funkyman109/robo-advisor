b85a458c4be73a855ef207cc561c3cc77a1ec333
funkyman109 <59669523+funkyman109@users.noreply.github.com>
2020-02-23 22:07:51 -0500

    Update rob_advisor.py

--- app/rob_advisor.py
+++ app/rob_advisor.py
@@ -2,7 +2,15 @@
 
 import requests
 import json
-
+def to_usd(price):
+    """
+    Converts a numeric value to usd-formatted string, for printing and display purposes.
+    Source: https://github.com/prof-rossetti/intro-to-python/blob/master/notes/python/datatypes/numbers.md#formatting-as-currency
+    Param: my_price (int or float) like 4000.444444
+    Example: to_usd(4000.444444)
+    Returns: $4,000.44
+    """
+    return f"${price:,.2f}" #> $12,000.71
 #info inputs
 
 request_url= "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo"
@@ -12,9 +20,13 @@ response = requests.get(request_url)
 #print(response.status_code)
 #print(response.text)
 
-parsed_respone= json.loads(response)
+parsed_response= json.loads(response.text)
+last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+latest_close = to_usd(float(parsed_response['Time Series (Daily)']['2020-02-13']['4. close']))
 
 #info outputs
+
+
 
 
 # app/robo_advisor.py
@@ -25,8 +37,8 @@ print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
 print("REQUEST AT: 2018-02-20 02:00pm")
 print("-------------------------")
-print("LATEST DAY: 2018-02-20")
-print("LATEST CLOSE: $100,000.00")
+print("LATEST DAY:", last_refreshed)
+print("LATEST CLOSE:", latest_close)
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
 print("-------------------------")
