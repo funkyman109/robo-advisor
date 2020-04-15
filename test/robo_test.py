fa5a37d7d3f72df1ae29d5826597b7b585ec6c7d
funkyman109 <59669523+funkyman109@users.noreply.github.com>
2020-04-15 19:34:13 -0400

    Update robo_test.py

--- test/robo_test.py
+++ test/robo_test.py
@@ -1,5 +1,28 @@
-from app.robo_advisor import to_usd
+import csv
+import json
+import os
+import pandas as pd
+import re
+import sys
+import datetime
+
+from dotenv import load_dotenv
+import requests
+
+from app.robo_advisor import to_usd, get_response
 
 def test_to_usd():
     result = to_usd(3.47)
     assert result == f"${3.47:,.2f}"
+
+def test_getresponse():
+    symbol = "AAPL"
+    api_key = str(os.environ.get("ALPHAVANTAGE_API_KEY"))
+    #pull1 = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + ticker + "&apikey=" + apikey)
+
+    answer = get_response(symbol, api_key)
+
+    parsed_answer = json.loads(answer.text)
+    result = list(parsed_answer)
+
+    assert result == ['Meta Data', 'Time Series (Daily)']
