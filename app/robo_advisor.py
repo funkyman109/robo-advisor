a7b72f986fc820d12925d09491af8f99568efb56
funkyman109 <59669523+funkyman109@users.noreply.github.com>
2020-04-15 19:09:55 -0400

    Update robo_advisor.py

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -23,107 +23,110 @@ def to_usd(price):
     return f"${price:,.2f}" #> $12,000.71
 #info inputs
 
-Daytime = datetime.datetime.now()
 
-#api_key = os.environ.get("ALPHAVANTAGE_API_KEY")
-api_key = os.getenv("ALPHAVANTAGE_API_KEY", default="OOPS")
+if __name__ == "__main__":
 
-#taken from https://stackoverflow.com/questions/8761778/limiting-python-input-strings-to-certain-characters-and-lengths
-symbol = input("Please enter the ticker of your stock of choice: ")
-if not re.match("^[a-z]*$", symbol):
-    print("Error! Only letters a-z allowed!")
-    sys.exit()
-elif len(symbol) > 5:
-    print("Error! Only 5 characters allowed!")
-    sys.exit()
+    Daytime = datetime.datetime.now()
 
-request_url= f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={api_key}"
+    #api_key = os.environ.get("ALPHAVANTAGE_API_KEY")
+    api_key = os.getenv("ALPHAVANTAGE_API_KEY", default="OOPS")
 
-response = requests.get(request_url)
-#print(type(response))
-#print(response.status_code)
-#print(response.text)
+    #taken from https://stackoverflow.com/questions/8761778/limiting-python-input-strings-to-certain-characters-and-lengths
+    symbol = input("Please enter the ticker of your stock of choice: ")
+    if not re.match("^[a-z]*$", symbol):
+        print("Error! Only letters a-z allowed!")
+        sys.exit()
+    elif len(symbol) > 5:
+        print("Error! Only 5 characters allowed!")
+        sys.exit()
 
-parsed_response= json.loads(response.text)
-last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
-tsd = parsed_response['Time Series (Daily)']
-dates = list(tsd.keys())
-latest_date = dates[0]
-latest_close = to_usd(float(tsd[latest_date]['4. close']))
+    request_url= f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={api_key}"
 
-high_prices = []
-for date in dates:
-    high_price = float(tsd[date]['2. high'])
-    high_prices.append(high_price)
-recent_high = to_usd(max(high_prices))
+    response = requests.get(request_url)
+    #print(type(response))
+    #print(response.status_code)
+    #print(response.text)
 
-low_prices = []
-for date in dates:
-    low_price = float(tsd[date]['3. low'])
-    low_prices.append(low_price)
-recent_low = to_usd(min(low_prices))
+    parsed_response= json.loads(response.text)
+    last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+    tsd = parsed_response['Time Series (Daily)']
+    dates = list(tsd.keys())
+    latest_date = dates[0]
+    latest_close = to_usd(float(tsd[latest_date]['4. close']))
 
-#info outputs
-
-csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", "prices.csv")
-csv_headers = ['timestamp', 'open', 'high', 'low', 'close', 'volume']
-
-with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
-    writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
-    writer.writeheader() # uses fieldnames set above
+    high_prices = []
     for date in dates:
-      daily_prices= tsd[date]
-      writer.writerow({
-        "timestamp": date,
-        "open": daily_prices['1. open'],
-        "high": daily_prices['2. high'],
-        "low": daily_prices['3. low'],
-        "close": daily_prices['4. close'],
-        "volume": daily_prices['5. volume'],
-    })
-#making recommendation
-#pandas indformation taken from https://github.com/prof-rossetti/intro-to-python/blob/master/notes/python/packages/pandas.md
-stock_info = pd.read_csv(csv_file_path)
-#print(stock_info)
+        high_price = float(tsd[date]['2. high'])
+        high_prices.append(high_price)
+    recent_high = to_usd(max(high_prices))
 
-# retrieving variables from csv
-close = stock_info["close"][0]
-ll = stock_info["low"].min()
-hh = stock_info["high"].max()
-#print(close, ll, hh)
+    low_prices = []
+    for date in dates:
+        low_price = float(tsd[date]['3. low'])
+        low_prices.append(low_price)
+    recent_low = to_usd(min(low_prices))
 
-#creating a lower boundary in order to analyze volatility
-#idea taken from summer internship 
+    #info outputs
 
-lower_boundary = close * 0.75
-upper_boundary = close * 1.64
+    csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", "prices.csv")
+    csv_headers = ['timestamp', 'open', 'high', 'low', 'close', 'volume']
 
-if ll < lower_boundary or hh > upper_boundary:
-    rec = "Don't Buy."
-    reason = "Stock appears to be very volatile. Thus, it is a risk to your portfolio."
-else:
-    rec = "Buy it Baby."
-    reason = "Stock is not volatile and likely to continue on its current trend."
+    with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
+        writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
+        writer.writeheader() # uses fieldnames set above
+        for date in dates:
+            daily_prices= tsd[date]
+            writer.writerow({
+                "timestamp": date,
+                "open": daily_prices['1. open'],
+                "high": daily_prices['2. high'],
+                "low": daily_prices['3. low'],
+                "close": daily_prices['4. close'],
+                "volume": daily_prices['5. volume'],
+            })
+    #making recommendation
+    #pandas indformation taken from https://github.com/prof-rossetti/intro-to-python/blob/master/notes/python/packages/pandas.md
+    stock_info = pd.read_csv(csv_file_path)
+    #print(stock_info)
+
+    # retrieving variables from csv
+    close = stock_info["close"][0]
+    ll = stock_info["low"].min()
+    hh = stock_info["high"].max()
+    #print(close, ll, hh)
+
+    #creating a lower boundary in order to analyze volatility
+    #idea taken from summer internship 
+
+    lower_boundary = close * 0.75
+    upper_boundary = close * 1.64
+
+    if ll < lower_boundary or hh > upper_boundary:
+        rec = "Don't Buy."
+        reason = "Stock appears to be very volatile. Thus, it is a risk to your portfolio."
+    else:
+        rec = "Buy it Baby."
+        reason = "Stock is not volatile and likely to continue on its current trend."
 
 
-# app/robo_advisor.py
+    # app/robo_advisor.py
 
-print("-------------------------")
-print("SELECTED SYMBOL:", symbol)
-print("-------------------------")
-print("REQUESTING STOCK MARKET DATA...")
-print("REQUEST AT:", Daytime)
-print("-------------------------")
-print("LATEST DAY:", last_refreshed)
-print("LATEST CLOSE:", latest_close)
-print("RECENT HIGH:", recent_high)
-print("RECENT LOW:", recent_low)
-print("-------------------------")
-print("RECOMMENDATION:", rec)
-print("RECOMMENDATION REASON:", reason)
-print("-------------------------")
-print("writing data to csv file:", csv_file_path)
-print("-------------------------")
-print("HAPPY INVESTING!")
-print("-------------------------")
+    print("-------------------------")
+    print("SELECTED SYMBOL:", symbol)
+    print("-------------------------")
+    print("REQUESTING STOCK MARKET DATA...")
+    print("REQUEST AT:", Daytime)
+    print("-------------------------")
+    print("LATEST DAY:", last_refreshed)
+    print("LATEST CLOSE:", latest_close)
+    print("RECENT HIGH:", recent_high)
+    print("RECENT LOW:", recent_low)
+    print("-------------------------")
+    print("RECOMMENDATION:", rec)
+    print("RECOMMENDATION REASON:", reason)
+    print("-------------------------")
+    print("writing data to csv file:", csv_file_path)
+    print("-------------------------")
+    print("HAPPY INVESTING!")
+    print("-------------------------")
 
