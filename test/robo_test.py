8034cbe84f0cc657aae8d795a9d3c644228904f2
funkyman109 <59669523+funkyman109@users.noreply.github.com>
2020-04-15 20:04:01 -0400

    Update robo_test.py

--- test/robo_test.py
+++ test/robo_test.py
@@ -9,7 +9,7 @@ import datetime
 from dotenv import load_dotenv
 import requests
 
-from app.robo_advisor import to_usd, get_response
+from app.robo_advisor import to_usd, get_response, parsed_answer
 
 def test_to_usd():
     result = to_usd(3.47)
@@ -24,5 +24,17 @@ def test_getresponse():
 
     parsed_answer = json.loads(answer.text)
     result = list(parsed_answer)
+
+    assert result == ['Meta Data', 'Time Series (Daily)']
+
+def test_parsedanswer():
+    symbol = "AAPL"
+    api_key = str(os.environ.get("ALPHAVANTAGE_API_KEY"))
+    pull1 = requests.get(f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={api_key}")
+
+    #answer = get_response(symbol, api_key)
+
+    parsed = parsed_answer(pull1)
+    result = list(parsed)
 
     assert result == ['Meta Data', 'Time Series (Daily)']
